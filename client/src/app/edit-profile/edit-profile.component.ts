ef0e2ae8c12e7a90691e764169b7fdc7e4f9e6d6
BPongz <89505707+BPongz@users.noreply.github.com>
2021-11-01 13:08:45 +0700

    Fix Address and test Upload Button

--- client/src/app/edit-profile/edit-profile.component.ts
+++ client/src/app/edit-profile/edit-profile.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpEventType } from '@angular/common/http';
 import { Component, OnInit, Input } from '@angular/core';
 import { faAddressBook, faUserSecret, faArchive } from '@fortawesome/free-solid-svg-icons';
 
@@ -11,21 +12,22 @@ export class EditProfileComponent implements OnInit {
   faUserSecret = faUserSecret;
   faArchive = faArchive;
   imgUrl = '../../assets/images/userpic.png';
+  selectedFile: File = null as any;
+  percent = 0;
 
   @Input() userdetail:boolean = true;
   @Input() secure:boolean = false;
   @Input() myorder:boolean = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   doSomethingOnError(event:any) {
     event.target.src = '../../assets/images/userpic.png'
-
   }
-
+  //Change Page Section
   changePageDetail() {
     this.userdetail = true;
     this.secure = false;
@@ -40,5 +42,29 @@ export class EditProfileComponent implements OnInit {
     this.userdetail = false;
     this.secure = false;
     this.myorder = true;
+  }
+  //Upload User Profile Section
+  onFileSelected(event: any) {
+    this.selectedFile = <File>event.target.files[0];
+  }
+  onUpload() {
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    this.http.post('https://barbtood/upload', fd, {
+      reportProgress: true,
+      observe: 'events'
+    })
+    .subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        if (event.total) {
+          const total: number = event.total;
+          this.percent = Math.round(100 * event.loaded / total);
+      }
+        console.log('Upload Progress: ' + this.percent + '%');
+      } else if (event.type ===  HttpEventType.Response) {
+        console.log(event);
+      }
+      console.log(event);
+    });
   }
 }
