9b7af8fe4ffead33d0725a4da626be9ad48d79d9
NNICEEt <niceteerapat@gmail.com>
2021-11-10 17:11:28 +0700

    เพิ่ม Cart Services หน้าบ้าน

--- client/src/app/services/cart.service.ts
+++ client/src/app/services/cart.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Carts } from '../models/cart';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
 
-  constructor() { }
+  REST_API: string = 'http://localhost:3000/api/v1/carts/';
+  httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+
+  constructor(private httpClient: HttpClient) {}
+
+  //ADD Cart
+  addToCart(productId:string, quantity:number) {
+    return this.httpClient.post(this.REST_API, {productId, quantity});
+  }
+
+  //Get Cart
+  getCart() {
+    return this.httpClient.get<Carts>(this.REST_API);
+  }
+
+  //Update Cart
+  updateCart(cartId: string, quantity:number) {
+    return this.httpClient.put(`${this.REST_API}${cartId}`, {quantity});
+  }
+
+  //Delete  Cart
+  deleteCart(cartId: string) {
+    return this.httpClient.delete(`${this.REST_API}${cartId}`);
+  }
 }
